6cfb5a9625afa8c7ab6283d8e3ea3ccd912374fd
Obinna Nnadozie <josh.nnadozie@gmail.com>
2022-04-01 15:48:03 -0400

    Refactored the entire rock_paper_scissors game logic

--- RB_101/lesson_2/rock_paper_scissors_bonus.rb
+++ RB_101/lesson_2/rock_paper_scissors_bonus.rb
@@ -6,40 +6,40 @@ CHOICES = YAML.load_file('rock_paper_scissors_bonus.yml')
 Rock paper scissors bonus question includes adding 2 more options; adding spoke
 and lizard to the options to choose from.
 - A game of 2 players, player and computer
-- The player and computer chooses from the available choices and whoever is the
+- The player and computer chooses from the available choices
 - Ties are replayed, meaning someone must win each round else repeat
-- After 3 wins by either player, the player with >= 3 wwins
+- After 3 wins by either player, the player with >= 3 wins
 - The program ends
 =end
 
 =begin
 Pseudocode and problem deconstruction
-- Set a const VALID_CHOICES  array which will have all the choices to pick from
+- SET score_tracker = {} that will keep track of all the scores in real time
+- Set a const VALID_CHOICES array which will have all the choices to pick from
 - Define a method that displays a unique output, it will take string as input
-- Initialize a variable outside of the loop to capture player & computer choice
-- Ask the players to make a choice and validate from the VALID_CHOICES array
-  Break out of the loop if the player choice is valid else repeat
-- Use the Array#sample method to make a random choice from the VALID_CHOICES
-  and SET the return object to a variable computer_choice
-- Now you have both choices available, display what player and computer chose
+- Get player choice, and validate the input.
+- Get computer choice by using Array#sample method to pick from VALID CHOICES
+- Now you have both choices available, PRINT what player and computer chose
 - Validate to make sure that there are no ties, proceed if no ties, else repeat
 - Build your logic to determine the winner, refer to the logic below:
 - Define a winner method which wil determine the winner
-  The winner method will take 2 parameters, and will have a hash.
+  The winner method will take 3 parameters and a hash within.
   The key of the hash will be the possible moves a player can make(see choices)
   The value will be an array of the possible move a computer can make
-  IF computer move is part of the values of the key, player wins
-  ELSE computer wins
-- Define a method that will display result for each round, this method will
-  use the winner method defined above to make that decision
-- Define a method that will display the final result, this method will need 2
-  arguments, player score and computer score
--As an addition, I also configured a yaml file which serves as a hash, the key
- will be the player's input(first letter of the options) and values = full word
+  IF computer move is part of the values of the key, player wins then increment
+  player score in the score_tracker hash and display round result
+  ELSE computer wins - do same for computer
+- PRINT or display the champion by invoking the champion_method
+- Ask player if they want to play again, repeat if yes or break if no.
+- As an addition, I also configured a yaml file which serves as a hash, the key
+will be the player's input(first letter of the options) and values = full word
 =end
 
 # A constant variable used here as it acts as a global variable, it wont change
 VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+
+# A score_record hash that will keep track of the player & computer scores
+score_record = { player: 0, computer: 0 }
 
 # A prompt method that displays a unique message
 def prompt(message)
@@ -56,37 +56,45 @@ l => lizard
 sp => spock
 MSG
 
-# A method that displays results of winner
-def display_results(player_score, computer_score)
+# An increment method, that increments the score record based on name(winner)
+def increment(name, score_tracker)
+  score_tracker[name.to_sym] += 1
+end
+
+# A method that displays the champion or grand winner
+def display_champion(player, computer, score_tracker)
   puts " "
   prompt("Final =========== results:")
-  prompt("Player score = #{player_score}")
-  prompt("Computer score = #{computer_score}")
-  if player_score >= 3
-    prompt("Player is the grand winner! Yayyy..")
+  prompt("Player score = #{score_tracker[player.to_sym]}")
+  prompt("Computer score = #{score_tracker[computer.to_sym]}")
+  if score_tracker[player.to_sym] == 3
+    prompt("Player is the champion!!! Yayyy..")
     puts ' '
-  elsif computer_score >= 3
-    prompt("Computer is the grand winner!")
+  elsif score_tracker[computer.to_sym] == 3
+    prompt("Computer is the champion!")
     puts ' '
   end
 end
 
-# A method that displays each round's winner
-def display_round_winner(player_choice, computer_choice)
-  if winner(player_choice, computer_choice) == 'player_won'
+# A method that displays each round's winner.
+def display_round_winner(name_of_winner, round, score_tracker)
+  prompt("Round --- #{round} --- result:")
+  score_tracker.each { |k, val| prompt("#{k.to_s.capitalize} score = #{val}") }
+  if name_of_winner == 'player'
     prompt("Player won this round!")
   else
     prompt("Computer won this round!")
   end
+  puts ' '
 end
 
-# A method that determines the winner and returns a value
-def winner(player, computer)
+# A method that determines the winner, increments winner
+def decide_winner(player_choice, computer_choice, score_tracker, round)
   # A hash containing the player move as a key and a list of moves
   # that the player's move beats
   # If player chooses a key, and computer choosess any of the values
   # in the list then player has won, else computer wins
-  player_move = {
+  player_moves = {
     rock: ['scissors', 'lizard'],
     paper: ['spock', 'rock'],
     scissors: ['paper', 'lizard'],
@@ -94,82 +102,90 @@ def winner(player, computer)
     spock: ['rock', 'scissors']
   }
   # check the winner, arr is an array of possible computer moves
-  arr = player_move[player.to_sym]
-  if arr.include?(computer)
-    'player_won'
+  arr = player_moves[player_choice.to_sym]
+  if arr.include?(computer_choice)
+    increment('player', score_tracker)
+    display_round_winner('player', round, score_tracker)
   else
-    'computer_won'
+    increment('computer', score_tracker)
+    display_round_winner('computer', round, score_tracker)
   end
 end
 
-# Variable for player & computer choices & scores, why here? - variable scope
-player_choice = ''
-computer_choice = ''
-player_score = 0
-computer_score = 0
+# A method that gets and verfies player_choice
+def get_player_choice(choices_prompt)
+  player_choice = ' '
+  loop do
+    prompt(choices_prompt)
+    player_choice = gets.chomp.strip
+    if VALID_CHOICES.include?(CHOICES[player_choice])
+      player_choice = CHOICES[player_choice]
+      break
+    else
+      prompt("You made an invalid choice, please enter a valid choice:")
+      puts ' '
+    end
+  end
+  player_choice
+end
+
+# A method that gets computer choice
+def get_computer_choice
+  VALID_CHOICES.sample
+end
+
+# A method that resets the scores in the hash
+def reset(score_tracker)
+  # this expression transforms the hash values to 0
+  score_tracker.transform_values! { 0 }
+end
+
+# A method that asks the user if they want to play again
+def play_again?(score_tracker)
+  prompt("Do you want to play again?")
+  answer = gets.chomp
+  if answer.downcase.start_with?('y')
+    system "clear"
+    prompt("Welcome back!")
+    reset(score_tracker)
+    true
+  else
+    false
+  end
+end
 
 # Welcome message
 prompt("Welcome to rock, paper, scissors, lizard, spock game!")
-# The loop to begin the game
+
+# The loop to begin the game and controls its entire execution
 loop do
+  round = 1 # to keep track of each round
   # The until loop determines when the game should end
-  until player_score == 3 || computer_score == 3
+  until score_record.value?(3)
+    # Variable for player & computer choices & scores,
+    # Why here? - think variable scope
+    player_choice = ' '
+    computer_choice = ' '
+
     # This loop checks for ties between player and computer, we don't want ties
     loop do
-      # This loop is for validation of player_choice
-      loop do
-        # Use Array#join method to display the elements of valid choices
-        # prompt("Please make a choice: #{VALID_CHOICES.join(', ')}")
-        prompt(choices_prompt)
-        player_choice = gets.chomp.strip
-
-        # Validate user choice using Array#include method
-        if VALID_CHOICES.include?(CHOICES[player_choice])
-          break
-        else
-          prompt("You made an invalid choice, please enter a valid choice:")
-          puts ' '
-        end
-      end
-
-      # Computer plays next -Use Array#sample method to make a choice at random
-      computer_choice = VALID_CHOICES.sample
-      prompt("You chose: #{CHOICES[player_choice]}")
+      player_choice = get_player_choice(choices_prompt)
+      computer_choice = get_computer_choice()
+      prompt("Player chose: #{player_choice}")
       prompt("Computer chose: #{computer_choice}")
-      # Validate to make sure that there are no ties, else repeat until no ties
-      if CHOICES[player_choice] != computer_choice
-        # Display the winner of each round by calling the below method.
-        display_round_winner(CHOICES[player_choice], computer_choice)
-        puts " "
-        break
-      else
-        prompt("Seems we have a tie here, please make choice again:")
-        puts ' '
-      end
+      puts ' '
+      break if player_choice != computer_choice
+      prompt("Seems we have a tie here, please make choice again:")
+      puts ' '
     end
-    # The logic methods goes here, calls the winner method to decide winner
-    # Then increment whoever won
-    if winner(CHOICES[player_choice], computer_choice) == 'player_won'
-      player_score += 1
-    else
-      computer_score += 1
-    end
+    # invoke the decide winne method
+    decide_winner(player_choice, computer_choice, score_record, round)
+    round += 1
   end
 
-  # once done, display result
-  display_results(player_score, computer_score)
-
-  # Ask user if they want to play the game again? if yes, reset scores
-  prompt("Do you want to play the game again?")
-  answer = gets.chomp
-  if answer.downcase().start_with?('y')
-    player_score = 0
-    computer_score = 0
-    # This line clears the screen before starting over
-    system "clear"
-  else
-    break
-  end
+  # It's time to display champion!!!
+  display_champion('player', 'computer', score_record)
+  _round = 0 # reset round
+  break unless play_again?(score_record)
 end
-
-prompt("Thank you for playing the game, bye!!!")
+prompt("Thank you for playing!!! bye")
